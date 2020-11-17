81be9d3cbb8868a01ad67f0cbba17250cf4ef1ac
KengoA <kengo@hey.com>
2020-11-18 04:09:55 +0900

    added answers for 254, 256, 257

---
+++ Chapter-02/254.scm
@@ -0,0 +1,25 @@
+; Exercise 2.54: Two lists are said to be equal? if they contain equal elements arranged in the same order. 
+; For example,
+; (equal? '(this is a list) '(this is a list))
+; is true, but
+; (equal? '(this is a list) '(this (is a) list))
+; is false. To be more precise, we can define equal? recursively in terms of the basic eq? equality of symbols by
+; saying that a and b are equal? if they are both symbols and the symbols are eq?, or if they are both lists such that (car
+; a) is equal? to (car b) and (cdr a) is equal? to (cdr b).
+; Using this idea, implement equal? as a procedure.
+
+; WIP
+(define (equal? a b)
+    (if (eq? (car a) (car b))
+        (equal? (cdr a) (cdr b))
+        (#f)
+    )
+)
+
+; TEST
+(define a '(this is a list))
+(define b '(this is a list))
+(define c '(this is another list))
+
+(print (equal? a b))
+(print (equal? a c))
